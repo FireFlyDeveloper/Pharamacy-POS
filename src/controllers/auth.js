b692d9a70e2a56899ab014a6b46d4e8dc787a894
FireFlyDeveloper <saludeskimdev@gmail.com>
2025-12-14 00:30:55 +0800

    refactor: remove unused authentication methods and clean up routes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,30 +1,8 @@
-const { hash, compare } = require("../helpers/hash");
-const { register, login, getCurrentUser, updatePassword } = require("../models/auth");
+const { compare } = require("../helpers/hash");
+const { login } = require("../models/auth");
 const { generate } = require("../helpers/jwt");
 
 class AuthController {
-    async register(req, res) {
-        const { username, password } = req.body;
-
-        if (!username || !password)
-            return res.status(400).json({ message: "Username and password required" });
-
-        try {
-            const hashedPassword = await hash(password);
-
-            const result = await register(username, hashedPassword);
-
-            res.status(201).json({ user: result });
-        } catch (err) {
-            if (err.code === "23505") {
-                res.status(400).json({ message: "Username already exists" });
-            } else {
-                console.error(err);
-                res.status(500).json({ message: "Server error" });
-            }
-        }
-    }
-
     async login(req, res) {
         const { username, password } = req.body;
 
@@ -45,28 +23,6 @@ class AuthController {
             const token = generate({ id: user.id, username: user.username });
 
             res.json({ token });
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: "Server error" });
-        }
-    }
-
-    async changePassword(req, res) {
-        const { oldPassword, newPassword } = req.body;
-
-        if (!oldPassword || !newPassword)
-            return res.status(400).json({ message: "Old and new password required" });
-
-        try {
-            const user = await getCurrentUser(req.user.id);
-            const match = await compare(oldPassword, user.password);
-
-            if (!match) return res.status(400).json({ message: "Old password is incorrect" });
-
-            const hashedNewPassword = await hash(newPassword);
-            await updatePassword(hashedNewPassword, req.user.id);
-
-            res.json({ message: "Password changed successfully" });
         } catch (err) {
             console.error(err);
             res.status(500).json({ message: "Server error" });
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const { AuthController } = require("../controllers/auth");
-const { authenticateToken } = require("../middlewares/jwt");
 
 const router = express.Router();
 const auth = new AuthController();
 
-router.post("/register", auth.register);
 router.post("/login", auth.login);
-router.post("/change-password", authenticateToken, auth.changePassword);
 
 module.exports = router;
